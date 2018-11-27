258eeb914b623620292277bdf8350bb3f860021f
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2018-11-28 01:17:05 +0900

    vb application created

---
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,273 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기를 사용하여 수정하지 마십시오.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.VEFX = New System.Windows.Forms.TrackBar()
+        Me.LOW_EQ = New System.Windows.Forms.TrackBar()
+        Me.HIGH_EQ = New System.Windows.Forms.TrackBar()
+        Me.FILTER = New System.Windows.Forms.TrackBar()
+        Me.VOLUME = New System.Windows.Forms.TrackBar()
+        Me.EFFECT_ON_OFF = New System.Windows.Forms.Button()
+        Me.VEFX_CHANGE = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.EFFECTOR_TEXT = New System.Windows.Forms.TextBox()
+        CType(Me.VEFX, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LOW_EQ, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.HIGH_EQ, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.FILTER, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.VOLUME, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'VEFX
+        '
+        Me.VEFX.LargeChange = 1
+        Me.VEFX.Location = New System.Drawing.Point(312, 118)
+        Me.VEFX.Maximum = 6
+        Me.VEFX.Name = "VEFX"
+        Me.VEFX.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.VEFX.Size = New System.Drawing.Size(90, 239)
+        Me.VEFX.TabIndex = 0
+        Me.VEFX.Value = 3
+        '
+        'LOW_EQ
+        '
+        Me.LOW_EQ.LargeChange = 1
+        Me.LOW_EQ.Location = New System.Drawing.Point(429, 118)
+        Me.LOW_EQ.Maximum = 6
+        Me.LOW_EQ.Name = "LOW_EQ"
+        Me.LOW_EQ.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.LOW_EQ.Size = New System.Drawing.Size(90, 239)
+        Me.LOW_EQ.TabIndex = 1
+        Me.LOW_EQ.Value = 3
+        '
+        'HIGH_EQ
+        '
+        Me.HIGH_EQ.LargeChange = 1
+        Me.HIGH_EQ.Location = New System.Drawing.Point(547, 118)
+        Me.HIGH_EQ.Maximum = 6
+        Me.HIGH_EQ.Name = "HIGH_EQ"
+        Me.HIGH_EQ.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.HIGH_EQ.Size = New System.Drawing.Size(90, 239)
+        Me.HIGH_EQ.TabIndex = 2
+        Me.HIGH_EQ.Value = 3
+        '
+        'FILTER
+        '
+        Me.FILTER.LargeChange = 1
+        Me.FILTER.Location = New System.Drawing.Point(671, 118)
+        Me.FILTER.Maximum = 6
+        Me.FILTER.Name = "FILTER"
+        Me.FILTER.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.FILTER.Size = New System.Drawing.Size(90, 239)
+        Me.FILTER.TabIndex = 3
+        Me.FILTER.Value = 3
+        '
+        'VOLUME
+        '
+        Me.VOLUME.LargeChange = 1
+        Me.VOLUME.Location = New System.Drawing.Point(792, 118)
+        Me.VOLUME.Maximum = 6
+        Me.VOLUME.Name = "VOLUME"
+        Me.VOLUME.Orientation = System.Windows.Forms.Orientation.Vertical
+        Me.VOLUME.Size = New System.Drawing.Size(90, 239)
+        Me.VOLUME.TabIndex = 4
+        Me.VOLUME.Value = 3
+        '
+        'EFFECT_ON_OFF
+        '
+        Me.EFFECT_ON_OFF.Location = New System.Drawing.Point(112, 118)
+        Me.EFFECT_ON_OFF.Name = "EFFECT_ON_OFF"
+        Me.EFFECT_ON_OFF.Size = New System.Drawing.Size(103, 90)
+        Me.EFFECT_ON_OFF.TabIndex = 5
+        Me.EFFECT_ON_OFF.UseVisualStyleBackColor = True
+        '
+        'VEFX_CHANGE
+        '
+        Me.VEFX_CHANGE.Location = New System.Drawing.Point(112, 317)
+        Me.VEFX_CHANGE.Name = "VEFX_CHANGE"
+        Me.VEFX_CHANGE.Size = New System.Drawing.Size(103, 90)
+        Me.VEFX_CHANGE.TabIndex = 6
+        Me.VEFX_CHANGE.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox1.Location = New System.Drawing.Point(112, 36)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(103, 28)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "effect"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox2.Location = New System.Drawing.Point(112, 77)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(103, 28)
+        Me.TextBox2.TabIndex = 8
+        Me.TextBox2.Text = "on/off"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox3.Location = New System.Drawing.Point(112, 235)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.ReadOnly = True
+        Me.TextBox3.Size = New System.Drawing.Size(103, 28)
+        Me.TextBox3.TabIndex = 9
+        Me.TextBox3.Text = "VEFX"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox4.Location = New System.Drawing.Point(112, 276)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(103, 28)
+        Me.TextBox4.TabIndex = 10
+        Me.TextBox4.Text = "change"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox5.Location = New System.Drawing.Point(299, 77)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.ReadOnly = True
+        Me.TextBox5.Size = New System.Drawing.Size(103, 28)
+        Me.TextBox5.TabIndex = 11
+        Me.TextBox5.Text = "VEFX"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox6.Location = New System.Drawing.Point(416, 77)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.ReadOnly = True
+        Me.TextBox6.Size = New System.Drawing.Size(103, 28)
+        Me.TextBox6.TabIndex = 12
+        Me.TextBox6.Text = "low-EQ"
+        '
+        'TextBox7
+        '
+        Me.TextBox7.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox7.Location = New System.Drawing.Point(534, 77)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.ReadOnly = True
+        Me.TextBox7.Size = New System.Drawing.Size(103, 28)
+        Me.TextBox7.TabIndex = 13
+        Me.TextBox7.Text = "hi-EQ"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox8.Location = New System.Drawing.Point(658, 77)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.ReadOnly = True
+        Me.TextBox8.Size = New System.Drawing.Size(103, 28)
+        Me.TextBox8.TabIndex = 14
+        Me.TextBox8.Text = "filter"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBox9.Location = New System.Drawing.Point(779, 77)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.ReadOnly = True
+        Me.TextBox9.Size = New System.Drawing.Size(103, 28)
+        Me.TextBox9.TabIndex = 15
+        Me.TextBox9.Text = "volume"
+        '
+        'EFFECTOR_TEXT
+        '
+        Me.EFFECTOR_TEXT.Location = New System.Drawing.Point(389, 405)
+        Me.EFFECTOR_TEXT.Name = "EFFECTOR_TEXT"
+        Me.EFFECTOR_TEXT.ReadOnly = True
+        Me.EFFECTOR_TEXT.Size = New System.Drawing.Size(258, 35)
+        Me.EFFECTOR_TEXT.TabIndex = 16
+        Me.EFFECTOR_TEXT.Text = "EFFECTOR OFF"
+        Me.EFFECTOR_TEXT.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(989, 452)
+        Me.Controls.Add(Me.EFFECTOR_TEXT)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.VEFX_CHANGE)
+        Me.Controls.Add(Me.EFFECT_ON_OFF)
+        Me.Controls.Add(Me.VOLUME)
+        Me.Controls.Add(Me.FILTER)
+        Me.Controls.Add(Me.HIGH_EQ)
+        Me.Controls.Add(Me.LOW_EQ)
+        Me.Controls.Add(Me.VEFX)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "VEFX Slider"
+        CType(Me.VEFX, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LOW_EQ, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.HIGH_EQ, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.FILTER, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.VOLUME, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents VEFX As System.Windows.Forms.TrackBar
+    Friend WithEvents LOW_EQ As System.Windows.Forms.TrackBar
+    Friend WithEvents HIGH_EQ As System.Windows.Forms.TrackBar
+    Friend WithEvents FILTER As System.Windows.Forms.TrackBar
+    Friend WithEvents VOLUME As System.Windows.Forms.TrackBar
+    Friend WithEvents EFFECT_ON_OFF As System.Windows.Forms.Button
+    Friend WithEvents VEFX_CHANGE As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox6 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox7 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox8 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox9 As System.Windows.Forms.TextBox
+    Friend WithEvents EFFECTOR_TEXT As System.Windows.Forms.TextBox
+
+End Class
---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,520 @@
+﻿Public Class Form1
+    Public Shared effector_num As Integer = 1
+    Public Shared effector_on As Integer = 0
+    Public Shared echo_texts = New String() {"REVERB 3", "REVERB 2", "REVERB 1", "ECHO 1", "ECHO 2", "ECHO 3", "ECHO 4"}
+    Public Shared echo_ex_texts = New String() {"REVERB EX 3", "REVERB EX 2", "REVERB EX 1", "ECHO EX 1", "ECHO EX 2", "ECHO EX 3", "ECHO EX 4"}
+    Public Shared compressor_texts = New String() {"COMPRESSOR"}
+    Public Shared chorus_texts = New String() {"FLANGER 3", "FLANGER 2", "FLANGER 1", "CHORUS 1", "CHORUS 2", "CHORUS 3", "CHORUS 4"}
+    Public Shared distortion_texts = New String() {"GARGLE 3", "GARGLE 2", "GARGLE 1", "DISTORTION 1", "DISTORTION 2", "DISTORTION 3", "DISTORTION 4"}
+    Public Shared effector_slider As Integer = 3
+    Public Shared loweq_slider As Integer = 3
+    Public Shared hieq_slider As Integer = 3
+    Public Shared filter_slider As Integer = 3
+    Public Shared vol_slider As Integer = 3
+
+    Public Shared temp_file = New String() {}
+    Public Shared echo_file = New String() {
+    "",
+"#ECHO",
+"",
+"Preamp: 0dB",
+"",
+"",
+"",
+"Device: all",
+"Copy: L1=L R1=R",
+"",
+"",
+"Device: 스피커; speaker",
+"Copy: L1=L+RL R1=R+RR",
+"",
+"",
+"",
+"Device: all",
+"#DELAY SYSTEM<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"#L1 R1: BASS SPEAKER SYSTEM",
+"#L11 R11: UPPER SPEAKER SYSTEM",
+"",
+"Channel: L1 R1",
+"Preamp: 3dB",
+"",
+"Copy: L99=L1 R99=R1",
+"Channel: L99 R99",
+"GraphicEQ: 1 12; 160 12; 250 6; 2500 -6",
+"Delay: 5ms",
+"#",
+"# LEVEL 1: 83/17",
+"# LEVEL 2: 80/20",
+"# LEVEL 3: 75/25",
+"# LEVEL 4: 67/33",
+"Copy: L1=0.75*L1+0.25*L99 R1=0.75*R1+0.25*R99",
+"Copy: L11=L1 R11=R1",
+"#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"#SURROUND SYSTEM<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"Channel: L1 R1",
+"GraphicEQ: 1 0; 250 0; 251 -57; 40000 -57",
+"Delay: 0ms",
+"Preamp: -0dB",
+"Channel: L11 R11",
+"GraphicEQ: 1 -57; 250 -57; 251 0; 40000 0",
+"Delay: 0ms",
+"Preamp: -0dB",
+"",
+"#reverb source",
+"Copy: L2=R1 R2=L1 L12=R11 R12=L11",
+"Channel: L2 R2",
+"Delay: 20ms",
+"Preamp: -6dB",
+"Channel: L12 R12",
+"Delay: 20ms",
+"Preamp: -6dB",
+"",
+"#reverb only delay",
+"Channel: R12 R2",
+"Delay: 20ms",
+"",
+"Copy: R3=R2 L3=L2 R13=R12 L13=L12",
+"Channel: L3 R3",
+"GraphicEQ: 1 0; 160 0; 161 -57; 40000 -57",
+"Delay: 40ms",
+"Preamp: -9dB",
+"Channel: L13 R13",
+"GraphicEQ: 1 0; 400 0; 401 -57; 40000 -57",
+"Delay: 40ms",
+"Preamp: -9dB",
+"",
+"Copy: L4=R3 R4=L3 L14=R13 R14=L13",
+"Channel: L4 R4",
+"Delay: 80ms",
+"Preamp: -12dB",
+"Channel: L14 R14",
+"Delay: 80ms",
+"Preamp: -12dB",
+"",
+"Copy: L5=R4 R5=L4 L15=R14 R15=L14",
+"Channel: L5 R5",
+"Delay: 160ms",
+"Preamp: -15dB",
+"Channel: L15 R15",
+"Delay: 160ms",
+"Preamp: -15dB",
+"",
+"Copy: L6=R5 R6=L5 L16=R15 R16=L15",
+"Channel: L6 R6",
+"Delay: 320ms",
+"Preamp: -18dB",
+"Channel: L16 R16",
+"Delay: 320ms",
+"Preamp: -18dB",
+"",
+"#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"#MIXER<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"#upper speaker system",
+"Channel: L12 R12",
+"Preamp: -6dB		#set -57 to kill REVERB		12dB maximum",
+"Channel: L13 L14 L15 L16 R13 R14 R15 R16",
+"Preamp: 12dB		#set -57 to kill ECHO		12dB maximum",
+"Copy: L19=L11+L12+L13+L14+L15+L16 R19=R11+R12+R13+R14+R15+R16",
+"",
+"#bass speaker system",
+"Channel: L2 R2",
+"Preamp: -6dB		#set -57 to kill REVERB		12dB maximum",
+"Channel: L3 L4 L5 L6 R3 R4 R5 R6",
+"Preamp: 12dB		#set -57 to kill ECHO		12dB maximum",
+"Copy: L9=L1+L2+L3+L4+L5+L6 R9=R1+R2+R3+R4+R5+R6",
+"",
+"#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"",
+"",
+"Device: all",
+"Copy: L=L19+L9 R=R19+R9",
+"",
+"",
+"Device: 스피커; speaker",
+"Copy: RL=L19+L9 RR=R19+R9",
+"",
+""}
+
+    Public Shared echo_ex_file = New String() {
+        "",
+"#ECHO EX",
+"",
+"Preamp: 0dB",
+"",
+"",
+"",
+"Device: all",
+"Copy: L1=L R1=R",
+"",
+"",
+"Device: 스피커; speaker",
+"Copy: L1=L+RL R1=R+RR",
+"",
+"",
+"",
+"Device: all",
+"#DELAY SYSTEM<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"#L1 R1: BASS SPEAKER SYSTEM",
+"#L11 R11: UPPER SPEAKER SYSTEM",
+"",
+"Channel: L1 R1",
+"Preamp: 3dB",
+"",
+"Copy: L99=L1 R99=R1",
+"Channel: L99 R99",
+"GraphicEQ: 1 12; 160 12; 250 6; 2500 -6",
+"Delay: 0.5ms",
+"#",
+"# LEVEL 1: 83/17",
+"# LEVEL 2: 80/20",
+"# LEVEL 3: 75/25",
+"# LEVEL 4: 67/33",
+"Copy: L1=0.83*L1+0.17*L99 R1=0.83*R1+0.17*R99",
+"Copy: L11=L1 R11=R1",
+"#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"#SURROUND SYSTEM<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"Channel: L1 R1",
+"GraphicEQ: 1 0; 250 0; 251 -57; 40000 -57",
+"Delay: 0ms",
+"Preamp: -0dB",
+"Channel: L11 R11",
+"GraphicEQ: 1 -57; 250 -57; 251 0; 40000 0",
+"Delay: 0ms",
+"Preamp: -0dB",
+"",
+"#reverb source",
+"Copy: L2=R1 R2=L1 L12=R11 R12=L11",
+"Channel: L2 R2",
+"Delay: 20ms",
+"Preamp: -6dB",
+"Channel: L12 R12",
+"Delay: 20ms",
+"Preamp: -6dB",
+"",
+"#reverb only delay",
+"Channel: R12 R2",
+"Delay: 20ms",
+"",
+"#first reverb",
+"Copy: R3=R2 L3=L2 R13=R12 L13=L12",
+"Channel: L3 R3",
+"#GraphicEQ: 1 0; 160 0; 161 -57; 40000 -57",
+"Delay: 320ms",
+"Preamp: -9dB",
+"Channel: L13 R13",
+"#GraphicEQ: 1 0; 400 0; 401 -57; 40000 -57",
+"Delay: 320ms",
+"Preamp: -9dB",
+"",
+"Copy: L4=R3 R4=L3 L14=R13 R14=L13",
+"Channel: L4 R4",
+"Delay: 320ms",
+"Preamp: -12dB",
+"Channel: L14 R14",
+"Delay: 320ms",
+"Preamp: -12dB",
+"",
+"Copy: L5=R4 R5=L4 L15=R14 R15=L14",
+"Channel: L5 R5",
+"Delay: 320ms",
+"Preamp: -15dB",
+"Channel: L15 R15",
+"Delay: 320ms",
+"Preamp: -15dB",
+"",
+"Copy: L6=R5 R6=L5 L16=R15 R16=L15",
+"Channel: L6 R6",
+"Delay: 320ms",
+"Preamp: -18dB",
+"Channel: L16 R16",
+"Delay: 320ms",
+"Preamp: -18dB",
+"",
+"#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"#MIXER<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"#upper speaker system",
+"Channel: L12 R12",
+"Preamp: -6dB		#set -57 to kill REVERB		12dB maximum",
+"Channel: L13 L14 L15 L16 R13 R14 R15 R16",
+"Preamp: 0dB		#set -57 to kill ECHO		12dB maximum",
+"Copy: L19=L11+L12+L13+L14+L15+L16 R19=R11+R12+R13+R14+R15+R16",
+"",
+"#bass speaker system",
+"Channel: L2 R2",
+"Preamp: -6dB		#set -57 to kill REVERB		12dB maximum",
+"Channel: L3 L4 L5 L6 R3 R4 R5 R6",
+"Preamp: 0dB		#set -57 to kill ECHO		12dB maximum",
+"Copy: L9=L1+L2+L3+L4+L5+L6 R9=R1+R2+R3+R4+R5+R6",
+"",
+"#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"",
+"",
+"Device: all",
+"Copy: L=L19+L9 R=R19+R9",
+"",
+"",
+"Device: 스피커; speaker",
+"Copy: RL=L19+L9 RR=R19+R9",
+"",
+""}
+
+    Public Shared compressor_file = New String() {
+"",
+"#COMPRESSOR",
+"",
+"Preamp: 0dB",
+"",
+"",
+"",
+"Device: all",
+"Copy: L1=L R1=R",
+"",
+"",
+"Device: 스피커; speaker",
+"Copy: L1=L+RL R1=R+RR",
+"",
+"",
+"",
+"Device: all",
+"#DELAY SYSTEM<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"#L1 R1: BASS SPEAKER SYSTEM",
+"#L11 R11: UPPER SPEAKER SYSTEM",
+"",
+"Channel: L1 R1",
+"Preamp: 3dB",
+"",
+"Copy: L99=L1 R99=R1",
+"Channel: L99 R99",
+"GraphicEQ: 1 12; 160 12; 250 6; 2500 -6",
+"Delay: 0.5ms",
+"#",
+"# LEVEL 1: 83/17",
+"# LEVEL 2: 80/20",
+"# LEVEL 3: 75/25",
+"# LEVEL 4: 67/33",
+"Copy: L1=0.83*L1+0.17*L99 R1=0.83*R1+0.17*R99",
+"Copy: L11=L1 R11=R1",
+"#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"#SURROUND SYSTEM<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"Channel: L1 R1",
+"GraphicEQ: 1 0; 250 0; 251 -57; 40000 -57",
+"Delay: 0ms",
+"Preamp: -0dB",
+"Channel: L11 R11",
+"GraphicEQ: 1 -57; 250 -57; 251 0; 40000 0",
+"Delay: 0ms",
+"Preamp: -0dB",
+"",
+"#reverb source",
+"Copy: L2=R1 R2=L1 L12=R11 R12=L11",
+"Channel: L2 R2",
+"Delay: 20ms",
+"Preamp: -6dB",
+"Channel: L12 R12",
+"Delay: 20ms",
+"Preamp: -6dB",
+"",
+"#reverb only delay",
+"Channel: R12 R2",
+"Delay: 20ms",
+"",
+"Copy: R3=R2 L3=L2 R13=R12 L13=L12",
+"Channel: L3 R3",
+"GraphicEQ: 1 0; 160 0; 161 -57; 40000 -57",
+"Delay: 40ms",
+"Preamp: -9dB",
+"Channel: L13 R13",
+"GraphicEQ: 1 0; 400 0; 401 -57; 40000 -57",
+"Delay: 40ms",
+"Preamp: -9dB",
+"",
+"Copy: L4=R3 R4=L3 L14=R13 R14=L13",
+"Channel: L4 R4",
+"Delay: 80ms",
+"Preamp: -12dB",
+"Channel: L14 R14",
+"Delay: 80ms",
+"Preamp: -12dB",
+"",
+"Copy: L5=R4 R5=L4 L15=R14 R15=L14",
+"Channel: L5 R5",
+"Delay: 160ms",
+"Preamp: -15dB",
+"Channel: L15 R15",
+"Delay: 160ms",
+"Preamp: -15dB",
+"",
+"Copy: L6=R5 R6=L5 L16=R15 R16=L15",
+"Channel: L6 R6",
+"Delay: 320ms",
+"Preamp: -18dB",
+"Channel: L16 R16",
+"Delay: 320ms",
+"Preamp: -18dB",
+"",
+"#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"#MIXER<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"#upper speaker system",
+"Channel: L12 R12",
+"Preamp: -6dB		#set -57 to kill REVERB		12dB maximum",
+"Channel: L13 L14 L15 L16 R13 R14 R15 R16",
+"Preamp: -57dB		#set -57 to kill ECHO		12dB maximum",
+"Copy: L19=L11+L12+L13+L14+L15+L16 R19=R11+R12+R13+R14+R15+R16",
+"",
+"#bass speaker system",
+"Channel: L2 R2",
+"Preamp: -6dB		#set -57 to kill REVERB		12dB maximum",
+"Channel: L3 L4 L5 L6 R3 R4 R5 R6",
+"Preamp: -57dB		#set -57 to kill ECHO		12dB maximum",
+"Copy: L9=L1+L2+L3+L4+L5+L6 R9=R1+R2+R3+R4+R5+R6",
+"",
+"#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<",
+"",
+"",
+"",
+"Device: all",
+"Copy: L=L19+L9 R=R19+R9",
+"",
+"",
+"Device: 스피커; speaker",
+"Copy: RL=L19+L9 RR=R19+R9",
+"",
+""}
+
+    Public Sub rerun()
+        If effector_on = 0 Then
+            EFFECTOR_TEXT.Text = "EFFECTOR OFF"
+        Else
+            Select Case effector_num
+                Case 1
+                    EFFECTOR_TEXT.Text = compressor_texts(0)
+                Case 2
+                    EFFECTOR_TEXT.Text = echo_texts(effector_slider)
+                Case 3
+                    EFFECTOR_TEXT.Text = echo_ex_texts(effector_slider)
+                Case 4
+                    effector_num = 1
+                    EFFECTOR_TEXT.Text = compressor_texts(0)
+            End Select
+        End If
+
+        writetostuff(effector_num, effector_slider, loweq_slider, hieq_slider, filter_slider, vol_slider)
+    End Sub
+
+    Public Sub writetostuff(num As Integer, slider As Integer, slider2 As Integer, slider3 As Integer, slider4 As Integer, slider5 As Integer)
+        System.IO.File.Create("config.txt").Dispose()
+        If effector_on <> 0 Then
+            Select Case num
+                Case 1
+                    temp_file = compressor_file
+
+                Case 2
+                    temp_file = echo_file
+                    temp_file(100) = "Preamp: " & (slider - 3) * -2 & "dB		#set -57 to kill REVERB		12dB maximum"
+                    temp_file(102) = "Preamp: " & slider * 2 & "dB		#set -57 to kill ECHO		12dB maximum"
+                    temp_file(107) = "Preamp: " & (slider - 3) * -2 & "dB		#set -57 to kill REVERB		12dB maximum"
+                    temp_file(109) = "Preamp: " & slider * 2 & "dB		#set -57 to kill ECHO		12dB maximum"
+                    If slider >= 3 Then
+                        temp_file(1) = "#ECHO"
+                        temp_file(22) = "Preamp: 3dB"
+                    Else
+                        temp_file(1) = "#REVERB"
+                        temp_file(22) = "Preamp: 0dB"
+                    End If
+                Case 3
+                    temp_file = echo_ex_file
+                    temp_file(101) = "Preamp: " & (slider - 3) * -2 & "dB		#set -57 to kill REVERB		12dB maximum"
+                    temp_file(103) = "Preamp: " & slider * 2 - 12 & "dB		#set -57 to kill ECHO		12dB maximum"
+                    temp_file(108) = "Preamp: " & (slider - 3) * -2 & "dB		#set -57 to kill REVERB		12dB maximum"
+                    temp_file(110) = "Preamp: " & slider * 2 - 12 & "dB		#set -57 to kill ECHO		12dB maximum"
+                    If slider >= 3 Then
+                        temp_file(1) = "#ECHO EX"
+                        temp_file(22) = "Preamp: 3dB"
+                    Else
+                        temp_file(1) = "#REVERB EX"
+                        temp_file(22) = "Preamp: 0dB"
+                    End If
+            End Select
+            If slider5 >= 3 Then
+                temp_file(3) = "Preamp: " & slider5 - 3 & "dB"
+            Else
+                temp_file(3) = "Preamp: " & slider5 * 10 - 30 & "dB"
+            End If
+
+            temp_file(5) = "GraphicEQ: 1 " & If(slider2 >= 3, slider2 - 3, slider2 * 10 - 30) & "; 160 " & If(slider4 >= 3, slider4 - 3, slider4 * 10 - 30) & "; 250 0; 2500 " & If(slider3 >= 3, slider3 - 3, slider3 * 10 - 30)
+
+            System.IO.File.WriteAllLines("config.txt", temp_file)
+        End If
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        writetostuff(effector_num, effector_slider, loweq_slider, hieq_slider, filter_slider, vol_slider)
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox6_TextChanged(sender As Object, e As EventArgs) Handles TextBox6.TextChanged
+
+    End Sub
+
+    Private Sub EFFECTOR_TEXT_TextChanged(sender As Object, e As EventArgs) Handles EFFECTOR_TEXT.TextChanged
+
+    End Sub
+
+    Private Sub EFFECT_ON_OFF_Click(sender As Object, e As EventArgs) Handles EFFECT_ON_OFF.Click
+        If effector_on = 0 Then
+            effector_on = 1
+        Else
+            effector_on = 0
+        End If
+        rerun()
+    End Sub
+
+    Private Sub VEFX_CHANGE_Click(sender As Object, e As EventArgs) Handles VEFX_CHANGE.Click
+        If effector_on <> 0 Then
+            effector_num = effector_num + 1
+        End If
+        rerun()
+
+    End Sub
+
+    Private Sub VEFX_Scroll(sender As Object, e As EventArgs) Handles VEFX.Scroll
+        effector_slider = VEFX.Value
+        rerun()
+
+    End Sub
+
+    Private Sub VOLUME_Scroll(sender As Object, e As EventArgs) Handles VOLUME.Scroll
+        vol_slider = VOLUME.Value
+        rerun()
+    End Sub
+
+    Private Sub FILTER_Scroll(sender As Object, e As EventArgs) Handles FILTER.Scroll
+        filter_slider = FILTER.Value
+        rerun()
+    End Sub
+
+    Private Sub LOW_EQ_Scroll(sender As Object, e As EventArgs) Handles LOW_EQ.Scroll
+        loweq_slider = LOW_EQ.Value
+        rerun()
+    End Sub
+
+    Private Sub HIGH_EQ_Scroll(sender As Object, e As EventArgs) Handles HIGH_EQ.Scroll
+        hieq_slider = HIGH_EQ.Value
+        rerun()
+    End Sub
+End Class
